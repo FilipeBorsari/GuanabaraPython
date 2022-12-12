2d15b888663ac23e0fd8d20945fb2cb1d72f99be
FilipeBorsari <119851304+FilipeBorsari@users.noreply.github.com>
2022-12-11 22:09:08 -0300

    6 exercícios adicionais
    Exercícios a serem realizados

--- Ex025.py
+++ Ex025.py
@@ -1,14 +1,23 @@
 # Crie um programa que leia o nome completo de uma pessoa e mostre:
+# O nome com todas maiúsculas
 # O nome com todas minúsculas
-# Quantas letras sem considerar espaços
+# Quantas letras ao todo sem considerar espaços
 # quantas letras tem o primeiro nome
+nome = str(input('qual seu nome? ')).strip() #esse strip ja elimina possíveis espaçps inúteis
+tamanho = len(nome) - nome.count(' ') #Conta o tamanho do nome 'menos' os espaços
+lista = nome.split()
+pnome = len(lista[0])
+print ('Seu nome em maíusculo é: {} '.format(nome.upper()))
+print ('Seu nome em minúsculo é: {}'.format(nome.lower()))
+print('Seu nome Possui ao todo {} letras'.format(tamanho))
+print ('Seu primeiro nome é {[0]} e tem {} Letras '.format(lista,pnome))
 
 
-nome = str(input('qual seu nome? '))
-nomestrip = nome.strip()
-nomesplit = nome.split()
+#Solução do Guanabara:
 
+print('\nAnalisando seu nome...')
+print ('Seu nome em maiúsculas é {} '.format(nome.upper()))
+print ('Seu nome em minúsculas é {} '.format(nome.lower()))
+print ('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+print ('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
 
-print ('o nome com letras minuscula: {}'.format(nome.lower()))
-print ('Possui {} letras '.format(len(nomestrip)))
-print (len(nome.strip()))
---
+++ Ex029.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia uma frase pelo teclado e mostre:
+# quantas vezes aparece a letra "A"
+# Em que posição ela aparece a primeira vez.
+# Em que posição ela aparece a última vez.
