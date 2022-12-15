85ec53f8cbd97066045c783b924355575568f68a
FilipeBorsari <119851304+FilipeBorsari@users.noreply.github.com>
2022-12-14 21:03:20 -0300

    Ultimos ex do mundo 1

--- Ex037.py
+++ Ex037.py
@@ -1,3 +1,11 @@
 #Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento
 #para salários superiores a 1250, calcule um aumento de 10%
 #Para os inferiores ou iguais, o aumento é de 15%
+
+salario = float(input('Qual o salário? '))
+
+if salario > 1250:
+    salario = salario + 0.10*salario
+else:
+    salario = salario + 0.15*salario
+print ('O novo salário será de {}'.format(salario))
--- Ex038.py
+++ Ex038.py
@@ -1,2 +1,19 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário
 #se elas podem ou não formar um triângulo.
+print ('-='*20)
+print ('Bem vindo ao analisador de Triângulos')
+print ('-='*20)
+
+
+r1 = float(input('Informe o comprimento da Reta 1 '))
+r2 = float(input('Informe o comprimento da Reta 2 '))
+r3 = float(input('Informe o comprimento da Reta 3 '))
+
+#para um triangulo existir, a soma de 2 lados deve ser menor que o 3
+
+
+
+if r1+r2>r3 and r2+r3>r1 and r1+r3>r2 :
+    print('Essas retas podem formar um triângulo')
+else:
+    print ('Essas retas não podem formar um triângulo')
